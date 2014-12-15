b7cd6738ecf4594142584fd8106c8de24ee90bcc
Luke Schafer <luke.schafer@gmail.com>
2014-12-15 10:57:33 +1000

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
   });
 
   // Register tasks.
-  grunt.loadNpmTasks('grunt-jasmine-runner');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
 
   // Default task.
   grunt.registerTask('test', 'jasmine');
